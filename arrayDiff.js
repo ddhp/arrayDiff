3c162f984f5e86cc0a6fa939ba4c4edc42f5c4c5
jessechen <jessechen@kkbox.com>
2015-01-08 17:16:51 +0800

    update solution

--- arrayDiff.js
+++ arrayDiff.js
@@ -1,3 +1,17 @@
+function randomArrFixLength(length) {
+  var result = [],
+      count = length;
+  while (length > 0) {
+    var v;
+    do {
+      v = Math.ceil(Math.random() * 100000);
+    } while (result.indexOf(v) !== -1);
+    result.push(v);
+    length --;
+  }
+  return result;
+}
+
 // Diff two arrays
 // return an array with none-duplicated elements
 var arrayDiff = function (aArr, bArr) {
@@ -13,12 +27,12 @@ var arrayDiff = function (aArr, bArr) {
       lngArr,
       sameArr = [];
 
-  console.log('aArr before sort, ', aArr.join(','));
-  console.log('bArr before sort, ', bArr.join(','));
+  // console.log('aArr before sort, ', aArr.join(','));
+  // console.log('bArr before sort, ', bArr.join(','));
   aArr.sort(sortFunc);
   bArr.sort(sortFunc);
-  console.log('aArr after sort, ', aArr.join(','));
-  console.log('bArr after sort, ', bArr.join(','));
+  // console.log('aArr after sort, ', aArr.join(','));
+  // console.log('bArr after sort, ', bArr.join(','));
   shortArr = aArr.length < bArr.length ? aArr : bArr;
   lngArr = aArr.length >= bArr.length ? aArr : bArr;
 
@@ -35,31 +49,35 @@ var arrayDiff = function (aArr, bArr) {
   //  2) a_v smaller then b_v: this a_v is a diff, not possible to match any elements in lngArr in the future loop
   //      - push to `diffArr`
   //      - break current loop
-  //  3) a_v bigger than b_v: continue to try next one
+  //  3) a_v bigger than b_v: continue to try next one, or if it's the last index of b, than push it to diffArr
   //     
   for (var a_i = 0, b_l = shortArr.length; a_i < b_l; a_i ++) {
     var a_v = shortArr[a_i];
     for (var b_i = b_i_c, b_l = lngArr.length; b_i < b_l; b_i ++) {
       var b_v = lngArr[b_i];
-      console.log('a_v = ' + a_v + ' and b_v = ' + b_v);
+      // console.log('a_v = ' + a_v + ' and b_v = ' + b_v);
       
       if (a_v === b_v) {
         // lngChecked.push[b_i];
         sameArr.push(a_v);
         b_i_c = b_i + 1;
-        console.log('b_i_c: ', b_i_c);
-        console.log('pushed to sameArr', sameArr);
+        // console.log('b_i_c: ', b_i_c);
+        // console.log('pushed to sameArr', sameArr);
         break;
       }
       // if a_v bigger than b_v, push a_v to diffArr
       if (a_v < b_v) {
         diffArr.push(a_v);
-        console.log('pushed to diff', diffArr);
+        // console.log('pushed to diff', diffArr);
         break;
       }
       // if a_v smaller than b_v, try next one
       if (a_v > b_v) {
-        continue;
+        if (!(b_i === b_l -1)) {
+          continue;
+        } else {
+          diffArr.push(a_v);
+        }
       }
     }
   }
@@ -71,11 +89,55 @@ var arrayDiff = function (aArr, bArr) {
     var v = sameArr[i];
     lngArr.splice(lngArr.indexOf(v), 1);
   }
-  console.log('lngArr after splice same elements: ', lngArr);
+  // console.log('lngArr after splice same elements: ', lngArr);
   return diffArr.concat(lngArr).sort(sortFunc);
 }
 
-var x = [1,3,93,4,8,5,15,14];
-var y = [1,8,93,2,9,5,17,14, 27, 44, 16];
-var result = arrayDiff(x, y);
-console.log('diff result: ', result);
+
+var x = randomArrFixLength(10000);
+var y = randomArrFixLength(20000);
+var startTime = new Date().getTime();
+var diffArr = arrayDiff(x, y.slice());
+var timeTook = new Date().getTime() - startTime;
+console.log('a_1 length: ', diffArr.length);
+console.log('Time took: ', timeTook);
+
+var sortFunc = function (a, b) {
+  return a -b;
+};
+var startTime = new Date().getTime();
+var sX = x.sort(sortFunc);
+var sY = y.sort(sortFunc);
+var differentArr = [];
+for (var i = 0, l = sX.length; i < l; i ++) {
+  var v = sX[i];
+  if (sY.indexOf(v) < 0) {
+    differentArr.push(v);
+  }
+}
+for (var i = 0, l = sY.length; i < l; i ++) {
+  var v = sY[i];
+  if (sX.indexOf(v) < 0) {
+    differentArr.push(v);
+  }
+}
+var timeTook = new Date().getTime() - startTime;
+differentArr = differentArr.sort(sortFunc);
+console.log('a_2 length: ', differentArr.length);
+console.log('Time took: ', timeTook);
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+console.log('is the same? ', arraysEqual(diffArr, differentArr));
